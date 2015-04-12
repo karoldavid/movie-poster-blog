fd4c27826c72f5ab6a3d3c3c0566da88b3bccc72
lorak <k.zysk@zoho.com>
2015-04-12 22:40:59 +0200

    add new json data for 8 new poster

--- js/movieposterBuilder.js
+++ js/movieposterBuilder.js
@@ -4,9 +4,10 @@ var posters = {
       "title" : { "PL" : "Dziecko Rosemary", "EN" : "Rosemary's Baby", "GER" : "Rosemaries Baby" },
       "director": "Roman Polanski",
       "artist": "",
-      "year" : 1984,
+      "year" : 1968,
       "size" : [200, 100],
       "description" : "Rosemary's Baby is a 1968 American psychological horror film written and directed by Roman Polanski, based on the bestselling 1967 novel Rosemary's Baby by Ira Levin.",
+      "www" : "https://en.wikipedia.org/wiki/Rosemary%27s_Baby_%28film%29",
       "url" : "images/dziecko_rosemary.svg"
     },
     {
@@ -16,6 +17,7 @@ var posters = {
       "year" : 1975,
       "size" : [100, 200],
       "description" : "Night Moves is a 1975 mystery thriller film directed by Arthur Penn. It stars Gene Hackman, Jennifer Warren, Susan Clark, and features early career appearances by Melanie Griffith and James Woods.",
+      "www" : "http://en.wikipedia.org/wiki/Night_Moves_%281975_film%29",
       "url" : "images/w_mroku_nocy.svg"
     },
     {
@@ -25,10 +27,90 @@ var posters = {
       "year" : 1976,
       "size" : [200, 100],
       "description" : "Murder by Death is a 1976 American mystery comedy film with a cast featuring Eileen Brennan, Truman Capote, James Coco, Peter Falk, Alec Guinness, Elsa Lanchester, David Niven, Peter Sellers, Maggie Smith, Nancy Walker, and Estelle Winwood, written by Neil Simon and directed by Robert Moore.",
+      "www" : "http://en.wikipedia.org/wiki/Murder_by_Death",
       "url": "images/zabity_na_smierc.svg"
+    },
+    {
+      "title" : { "PL" : "Imie Rozy", "EN" : "The Name of the Rose", "GER" : "Im Namen der Rose", "IT" : "Il nome della rosa" },
+      "director": "Jean-Jacques Annaud",
+      "artist": "",
+      "year" : 1986,
+      "size" : [200, 100],
+      "description" : "The Name of the Rose is a 1986 film directed by Jean-Jacques Annaud, based on the book of the same name by Umberto Eco. Sean Connery is the Franciscan friar William of Baskerville and Christian Slater is his apprentice Adso of Melk, who are called upon to solve a deadly mystery in a medieval abbey.",
+      "www" : "http://en.wikipedia.org/wiki/The_Name_of_the_Rose_%28film%29",
+      "url": "images/imie_rozy.svg",
+    },
+    {
+      "title" : { "PL" : "Chora z Milosci", "EN" : "Malady of Love ", "GER" : "Krank vor Liebe", "FR" : "Maladie d'amour" },
+      "director": "Jacques Deray",
+      "artist": "",
+      "year" : 1987,
+      "size" : [200, 100],
+      "description" : "Malady of Love (French: Maladie d'amour) is a 1987 French drama romance film, directed by Jacques Deray.",
+      "www" : "http://en.wikipedia.org/wiki/Malady_of_Love",
+      "url": "images/chora_z_milosci.svg",
+    },
+    {
+      "title" : { "PL" : "Jak być kochaną", "EN" : "How to be Loved" },
+      "director": "Wojciech Has",
+      "artist": "",
+      "year" : 1963,
+      "size" : [200, 100],
+      "description" : "How to be Loved (Polish: Jak być kochaną), a Polish film released in 1963, directed by Wojciech Has. The film, based on a novel of the same name by Kazimierz Brandys, examines the emotional casualties of war, which is perhaps the central theme of the Polish Film School. On a deeper level, the film manages to construct a personal tragedy that results from a struggle of egoism and cowardice versus devotion and courage.",
+      "www" : "http://en.wikipedia.org/wiki/How_to_Be_Loved",
+      "url": "images/jak_byc_kochana.svg",
+    },
+    {
+      "title" : { "PL" : "Mona Lisa", "EN" : "Mona Lisa", "GER" : "Mona Lisa" },
+      "director": "Neil Jordan",
+      "artist": "",
+      "year" : 1986,
+      "size" : [200, 100],
+      "description" : "Mona Lisa is a 1986 British neo-noir mystery drama about an ex-convict who becomes entangled in the dangerous life of a high-class call girl. The film was written by Neil Jordan and David Leland, and directed by Jordan. It was produced by HandMade Films and stars Bob Hoskins, Cathy Tyson and Michael Caine.",
+      "www" : "http://en.wikipedia.org/wiki/Mona_Lisa_%28film%29",
+      "url": "images/mona_lisa.svg",
+    },
+    {
+      "title" : { "PL" : "Mewy" },
+      "director": "Jerzego Passendorfera",
+      "artist": "",
+      "year" : 1986,
+      "size" : [200, 100],
+      "description" : "Mewy (Mewy /fragmenty życiorysu/) – polski dramat obyczajowy z 1986 roku w reż. Jerzego Passendorfera, na motywach powieści Stanisława Goszczurnego pod tym samym tytułem.",
+      "www" : "http://pl.wikipedia.org/wiki/Mewy_%28film%29",
+      "url": "images/mewy.svg",
+    },
+    {
+      "title" : { "PL" : "Powrót na Ziemię" },
+      "director": "Stanisław Jędryka",
+      "artist": "",
+      "year" : 1966,
+      "size" : [100, 200],
+      "description" : "Po przeżyciach wojennych Stefan nie potrafi wrócić do normalnego życia. Wciąż wspomina okupację, kiedy wraz z żoną uciekł z transportu",
+      "www" : "http://pl.wikipedia.org/wiki/Powr%C3%B3t_na_ziemi%C4%99",
+      "url": "images/powrot_na_ziemie.svg",
+    },
+    {
+      "title" : { "PL" : "Szkoda Twoich Lez", "EN" : "Tears won't Help" },
+      "director": "Lenartowicz Stanislaw",
+      "artist": "",
+      "year" : 1966,
+      "size" : [200, 100],
+      "description" : "Po przeżyciach wojennych Stefan nie potrafi wrócić do normalnego życia. Wciąż wspomina okupację, kiedy wraz z żoną uciekł z transportu",
+      "www" : "http://pl.wikipedia.org/wiki/Powr%C3%B3t_na_ziemi%C4%99",
+      "url": "images/szkoda_twoich_lez.svg",
+    },
+    {
+      "title" : { "PL" : "Wierna Rzeky" },
+      "director": "Tadeusz Chmielewski",
+      "artist": "",
+      "year" : 1983,
+      "size" : [100, 200],
+      "description" : "Wierna rzeka (film 1983) is a Polish historical film directed by Tadeusz Chmielewski. It was produced in 1983 and released in 1987.",
+      "www" : "http://en.wikipedia.org/wiki/Wierna_rzeka_%281983_film%29",
+      "url": "images/wierna_rzeky.svg",
     }]
 };
-
 
 posters.display = function() {
   for (poster in posters.posters) {
