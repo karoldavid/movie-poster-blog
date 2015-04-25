8fe850438a72964afcf99e13c806520d530a78fc
lorak <k.zysk@zoho.com>
2015-04-25 21:11:41 +0200

    remove size in imd data src

--- js/helper.js
+++ js/helper.js
@@ -2,7 +2,7 @@ var HTMLposterStart = '<figure class="poster-entry"></figure>';
 var HTMLposterTitle = '<a href="#">%data%</a>';
 var HTMLposterDates = '<div class="date-text">%data%</div>';
 var HTMLposterDescription = '<p class="description"><br>%data%</p>';
-var HTMLposterImageURL = '<picture><img class="lazy poster" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-original="images/posters/%data%-800_large_1x.jpeg" width="%width%"></picture>';
+var HTMLposterImageURL = '<picture><img class="lazy poster" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-original="images/posters/%data%-large_1x.jpeg" width="%width%"></picture>';
 var HTMLposterCaption = '<figcaption>%data%</figcaption>';
 var HTMLposterWWW = '<p class="www"><a href="#" target="_blank"> >>> </a></p>';
 
