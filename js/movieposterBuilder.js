62384f5847fb8d3a2332b942bedcdae6f6721629
lorak <k.zysk@zoho.com>
2015-07-08 22:28:58 +0200

    implement knockoutjs and search bar

--- js/movieposterBuilder.js
+++ js/movieposterBuilder.js
@@ -1,105 +1,9 @@
-$(function(){
+$(function(posters) {
 
-  var model = {
-    init: function() {
-      if (!localStorage.posters) {
-        localStorage.posters = JSON.stringify(posters.posters);
-      }
-    },
-    getAll: function() {
-      return JSON.parse(localStorage.posters);
-    },
-    getAllArtists: function() {
-      var artistNames = [],
-          posters = JSON.parse(localStorage.posters).posters;
-      for (poster in posters) {
-          artistNames.push(posters[poster].artist); 
-      }
-      return $.unique(artistNames);
-    },
-    set: function(posters) {
-      localStorage.posters = JSON.stringify(posters);
-    }
-  };
+    var initialPosters = posters;
 
-  var octopus = {
-    init: function() {
-      model.init();
-      view.init();
-      sortView.init();
-    },
-    getPosters: function() {
-      return model.getAll();
-    },
-    sortByArtist: function() {
-      var sortable = [],
-          posters = model.getAll();
 
-      for (poster in posters) {
-          sortable.push([posters[poster].artist, posters[poster]]); 
-      }
-
-      sortable.sort(function(a,b) { return a[1].artist.toLowerCase() > b[1].artist.toLowerCase()});
-      
-      var sortedByArtist = [];
-      for ( s in sortable) {
-         sortedByArtist.push(sortable[s][1]);
-      }
-      model.set(sortedByArtist);
-      return model.getAll();
-    },
-    sortPosters: function() {
-      var posters = model.getAll().posters,
-          list = model.getAllArtists();
-
-      var mapped = list.map(function(el, i) {
-        return { index: i, value: el.toLowerCase() };
-      })
-
-      // sorting the mapped array containing the reduced values
-      mapped.sort(function(a, b) {
-        return +(a.value > b.value) || +(a.value === b.value) - 1;
-      });
-
-      // container for the resulting order
-      var result = mapped.map(function(el){
-        return list[el.index];
-      });
-      console.log(result);
-      },
-      reverse: function() {
-         model.set(model.getAll().reverse());
-        return model.getAll();
-      }
-    };
-
-    var view = {
-      init: function() {
-        this.posterElem = $('#poster');
-        view.render();
-      },
-      //strips image name from filename and adds it as alt text to all images
-      addAltText: function() {
-        $(document).ready(function() {
-          $('img').each(function(){
-            var $img = $(this),
-              filename = $img.attr('data-original') || $img.attr('src'),
-              imageName = filename.substring(7, filename.lastIndexOf('.'));
-              $img.attr('alt', imageName);
-              //console.log(imageName);
-          });
-        });
-      },
-      render: function() {
-        var landscape = 800,
-            portrait = 600,
-            width = landscape,
-            //posters = octopus.getPosters().posters,
-            posters = octopus.sortByArtist();
-      
-
-        for (poster in posters) {
-          var url = posters[poster].url,
+    /* var url = posters[poster].url,
               title = posters[poster].title['PL'],
               artist = posters[poster].artist,
               x = posters[poster].size[0],
@@ -119,68 +23,77 @@ $(function(){
                          //+ '<a href="#">' + title + '</a>'
                          //+ '<div class="date-text">%data%</div>'
                          + '<p class="description"><br>' + description + '</p>'
-                         + '<p class="www"><a href="' + www +'" target="_blank"> >>> </a></p></figure>';
+                         + '<p class="website"><a href="' + www +'" target="_blank"> >>> </a></p></figure>';
            
-              this.posterElem.append(elem);
-        }
-        view.addAltText();
-      }
+              this.posterElem.append(elem);*/
+
+
+    var Poster = function(data) {
+      var portrait = 600,
+          landscape = 800;
+
+      this.artist = data.artist;
+      this.director = data.director;
+      this.description = data.description;
+      this.src = 'images/posters/' + data.src+ '-large_1x.jpeg';
+      this.title_pl = data.title['PL'] || "";
+      this.title_en = data.title['EN'] || "";
+      this.website= data.website;
+      this.year_movie = data.year['movie'];
+      this.x = data.size[0];
+      this.y = data.size[1];
+      this.width = this.x > this.y ? landscape : portrait;
+
+      this.figcaption = ko.pureComputed(function() {
+          return this.title_pl + ' (Poster designer: ' + this.artist + '; ' + this.x + 'x' + this.y + ')';
+      }, this);
+
+
+      this.searchQuery = ko.pureComputed(function() {
+        return this.title_en + ", " + this.title_pl + ", " + this.artist + ", " + this.director + ", " + this.year_movie;
+      }, this);
+
     };
 
-    var sortView = {
-      init: function() {
-        this.posterElems = $('.poster-entry');
+    var ViewModel = function() {
 
-        this.reverseButtonElem = document.getElementById('reverse'); //$('#reverse');
-        this.posters = model.getAll();
-        console.log(this.reverseButtonElem);
-        this.reverseButtonElem.addEventListener('click', function(){
-            this.posters = octopus.reverse();
-            //alert("reverse order");
-            sortView.render();
-        });
-        sortView.render();
-      },
-      render: function() {
-        //console.log(posterElems[0].childNodes[0]);
-        var pic  = this.posterElems.find('picture'),
-            fig  = this.posterElems.find('figcaption'),
-            desc = this.posterElems.find('.description'),
-            href  = this.posterElems.find('.www a'),
-            length = this.posters.length,
-            landscape = 800,
-            portrait = 600,
-            width = landscape;
+        var self = this;
 
-        console.log('----------------');
+        self.query = ko.observable("");
+
+         self.chosenPosterId = ko.observable("");
+
+        // Build poster list
+        self.posterList = ko.observableArray(ko.utils.arrayMap(initialPosters, function(posterItem) {
+            return new Poster(posterItem)
+        }));
+
+        // Sort poster list by name property
+        self.posterList().sort(function(left, right) {
+            return left.title_en === right.title_en ? 0 : (left.title_en < right.title_en ? -1 : 1)
+        })
         
-        for (var i = 0; i < length; i++) {
-          var url         = this.posters[i].url,
-              title       = this.posters[i].title['PL'],
-              artist      = this.posters[i].artist,
-              x           = this.posters[i].size[0],
-              y           = this.posters[i].size[1],
-              description = this.posters[i].description,
-              www         = this.posters[i].www;
-
-          if (this.posters[i].size[0] > this.posters[i].size[1]) {
-            width = landscape;
-          } else {
-            width = portrait;
-          }
-
-          console.log(pic[i].innerHTML);
-          pic[i].innerHTML = '<img class="lazy poster" src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-original="images/posters/' + url + '-large_1x.jpeg" width="' + width + '">'
-          console.log(fig[i].innerText);
-          fig[i].innerText = title + " (Poster designer: " + artist + "; " + x + "x" + y + ")"
-          console.log(desc[i].innerText);
-          desc[i].innerText = description;
-          console.log(href[i].href);
-          href[i].href = www;
-          console.log('----------------');
+        // Reverse poster list order
+        self.reverseList = function() {
+            self.posterList.reverse();
         }
-      }
-    };
+        
+        self.goToPoster = function(poster) {
+            self.chosenPosterId(poster != self.chosenPosterId() ? poster : "");
+        };
 
-  octopus.init();
-});
+        //Filter location list and return search result
+        self.searchResults = ko.computed(function() {
+            var search = self.query().toLowerCase();
+            return ko.utils.arrayFilter(self.posterList(), function(poster) {
+                return poster.searchQuery().toLowerCase().indexOf(search) >= 0;
+            });
+        });
+        
+    };
+    
+
+    ko.applyBindings(new ViewModel());
+}(posters.posters));
+
+
